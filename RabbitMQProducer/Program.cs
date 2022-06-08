b44c83d1e50cabe0f69fc13ac0d05d9a7716a676
irwankurniadi <irwan.kurniadi@si.ukdw.ac.id>
2022-06-08 12:51:17 +0700

    first commit

---
+++ RabbitMQProducer/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory {
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare("demo-queue", durable:true, exclusive:false, autoDelete:false, arguments:null);
+var message = new {Name="Producer", Message="Hello Rabbit MQ Irwan !"};
+var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+channel.BasicPublish("", "demo-queue",null,body);
